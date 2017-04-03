0104d99ba4eb0938dbe0b9f8ab4545f8c0429ba1
Bill Wang <bill.wang@myob.com>
2017-04-03 21:26:19 +1000

    add a typo to test

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('/', function(request, response) {
 })
 
 app.listen(app.get('port'), function() {
-  console.log ("Node app is running at localhost:" + app.get('port'))
+  console.logs("Node app is running at localhost:" + app.get('port'))
 })
